d9e7957bb743da0b7ee489753a589e7616545bfc
Daniel Wendisch <daniel.wendisch@gmail.com>
2024-10-02 12:24:24 +0200

    tons of things

--- QC_dataset_setup_hub_02.R
+++ QC_dataset_setup_hub_02.R
@@ -3,19 +3,18 @@
 # as separate assays
 # 
 
-
 ## inputs: 
-#     path_cellranger_output <- paste(sep = "\\", path_raw_data_file, "hub_01_outs\\multi\\count\\raw_feature_bc_matrix.h5")
-#     path_cellbender_output <-  paste(sep = "\\", path_raw_data_file,"cellbender\\hub_01\\hub_01_cellbender_corrected_filtered_seurat.h5")
+#     path_cellranger_output <- paste(sep = "\\", path_raw_data_file, "hub_02_outs\\multi\\count\\raw_feature_bc_matrix.h5")
+#     path_cellbender_output <-  paste(sep = "\\", path_raw_data_file,"cellbender\\hub_02\\hub_02_cellbender_corrected_filtered_seurat.h5")
 #     convert_vector <- c("CMO308"="EC_UCSFi001-A",
 #                     "CMO307"="EC_BIHi250-A",
 #                     "CMO306"="EC_BIHi005-A",
 #                     "CMO305"="EC_BIHi001-B",
 #                     "CMO309"="EC_BIHi001-A")
 # 
-#     tag_calls_summary <- read_csv(paste(sep = "\\", path_raw_data_file,  "hub_01_outs/multi/multiplexing_analysis/tag_calls_summary.csv"))
-#     tag_calls_per_cell <- read_csv(paste(sep = "\\", path_raw_data_file, "hub_01_outs/multi/multiplexing_analysis/tag_calls_per_cell.csv"))
-#     assignment_confidence_table <- read_csv(paste(sep = "\\",path_raw_data_file,  "hub_01_outs/multi/multiplexing_analysis/assignment_confidence_table.csv"))
+#     tag_calls_summary <- read_csv(paste(sep = "\\", path_raw_data_file,  "hub_02_outs/multi/multiplexing_analysis/tag_calls_summary.csv"))
+#     tag_calls_per_cell <- read_csv(paste(sep = "\\", path_raw_data_file, "hub_02_outs/multi/multiplexing_analysis/tag_calls_per_cell.csv"))
+#     assignment_confidence_table <- read_csv(paste(sep = "\\",path_raw_data_file,  "hub_02_outs/multi/multiplexing_analysis/assignment_confidence_table.csv"))
 
 ## outputs:
 # BPCells matrix
@@ -35,14 +34,11 @@
 #                         "_logfc.threshold_",logfc_threshold,
 #                         "_max.cells.per.ident_",max_cells_,
 #                         ".csv"))
-# write_rds(seurat_obj, "intermediate_data/QC_dataset_setup_hub_01.rds")
-
-
-
+# write_rds(seurat_obj, "intermediate_data/QC_dataset_setup_hub_02.rds")
 
 
 # set to calc_bp_cells to TRUE if BPCells matrices should be recalculated and saved
-calc_bp_cells <- T
+calc_bp_cells <- TRUE
 ###----------------------------------------------------------------------------------------------------
 library(readr)
 library(dplyr)
@@ -69,21 +65,22 @@ library(rhdf5)
 
 #set paths for dataset-associated files
 ###----------------------------------------------------------------------------------------------------
-dataset_name <- "hub_01"
+dataset_name <- "hub_02"
 path_raw_data_file <- "..\\..\\raw_data\\cubi_cusco"
 
-path_cellranger_output <- paste(sep = "\\", path_raw_data_file, "hub_01_outs\\multi\\count\\raw_feature_bc_matrix.h5")
-path_cellbender_output <-  paste(sep = "\\", path_raw_data_file,"cellbender\\hub_01\\hub_01_cellbender_corrected_filtered_seurat.h5")
+path_cellranger_output <- paste(sep = "\\", path_raw_data_file, "hub_02_outs\\multi\\count\\raw_feature_bc_matrix.h5")
+path_cellbender_output <-  paste(sep = "\\", path_raw_data_file,"cellbender\\hub_02\\hub_02_cellbender_corrected_filtered_seurat.h5")
 
 ## set hashtag conversion
-convert_vector <- c("CMO302"="hIO_BIHi005-A",
-                    "CMO301"="hIO_BIHi001-B",
-                    "CMO303"="hIO_BIHi250-A",
-                    "CMO304"="hIO_UCSFi001-A")
+convert_vector <- c("CMO308"="EC_UCSFi001-A",
+                    "CMO307"="EC_BIHi250-A",
+                    "CMO306"="EC_BIHi005-A",
+                    "CMO305"="EC_BIHi001-B",
+                    "CMO309"="EC_BIHi001-A")
 
-tag_calls_summary <- read_csv(paste(sep = "\\", path_raw_data_file,  "hub_01_outs/multi/multiplexing_analysis/tag_calls_summary.csv"))
-tag_calls_per_cell <- read_csv(paste(sep = "\\", path_raw_data_file, "hub_01_outs/multi/multiplexing_analysis/tag_calls_per_cell.csv"))
-assignment_confidence_table <- read_csv(paste(sep = "\\",path_raw_data_file,  "hub_01_outs/multi/multiplexing_analysis/assignment_confidence_table.csv"))
+tag_calls_summary <- read_csv(paste(sep = "\\", path_raw_data_file,  "hub_02_outs/multi/multiplexing_analysis/tag_calls_summary.csv"))
+tag_calls_per_cell <- read_csv(paste(sep = "\\", path_raw_data_file, "hub_02_outs/multi/multiplexing_analysis/tag_calls_per_cell.csv"))
+assignment_confidence_table <- read_csv(paste(sep = "\\",path_raw_data_file,  "hub_02_outs/multi/multiplexing_analysis/assignment_confidence_table.csv"))
 
 
 
@@ -153,14 +150,14 @@ seurat_obj[["uncorrected_RNA"]] <- dat_raw
 dat_cellbender <- NULL
 dat<- NULL
 gc()
-###-----------------------------------------------------------------------------------
+###---------------------------------------------------------------------------------------------------------
 
 
 
 
 
 # basic seurat processing for each assay
-###--------------------------------------------------------------------------------------------
+###-------------------------------------------------------------------------------------------------------
 for (assay_it in Assays(seurat_obj)) {
   DefaultAssay(seurat_obj) <- assay_it
   seurat_obj <- seurat_obj |> 
@@ -169,15 +166,15 @@ for (assay_it in Assays(seurat_obj)) {
   seurat_obj <- seurat_obj |>
     FindVariableFeatures(assay = assay_it) |> 
     ScaleData() 
-  #print("runPCA")
+  print("runPCA")
   seurat_obj <- seurat_obj |>
     RunPCA(assay = assay_it,reduction.name = paste0("pca.",assay_it)) 
-  #print("neighbors")
+  print("neighbors")
   seurat_obj <- seurat_obj |> FindNeighbors(dims = 1:30,
                                                   reduction = paste0("pca.",assay_it),
                                                   assay = assay_it) 
   
-  #print("clusters")
+  print("clusters")
   seurat_obj <- seurat_obj |>  FindClusters(
     resolution = 0.8,
     verbose = FALSE,
@@ -189,14 +186,16 @@ for (assay_it in Assays(seurat_obj)) {
     RunUMAP(dims = 1:30, reduction = paste0("pca.",assay_it),
             assay = assay_it,
             reduction.name =paste0("umap.",assay_it) )
+  
+  gc()
 }
-###-----------------------------------------------------------------------------------
+###-------------------------------------------------------------------------------------------------
 
 
 
 
 #add metadata
-###-----------------------------------------------------------------------------------
+###---------------------------------------------------------------------------------------------------
 
 # make metadata column: cell_line_cellranger 
 #-- match CMOxx to cell line name, adjusted feature_call column from cellranger multi
@@ -223,13 +222,10 @@ rownames(cell_barcodes) <- cell_barcodes$cell_barcode.y
 cell_barcodes <- cell_barcodes[,c("cell_line_cellranger","singlet_doublet_cellranger","cell_line_doublet_cellranger")]
 seurat_obj <- AddMetaData(seurat_obj,cell_barcodes)
 
-# saveRDS(
-#   object = seurat_obj,
-#   file = "intermediate_data/seurat_obj_hub_01_raw_and_corrected.Rds"
-# )
+
 
 # add cellbender cell probabilities
-corrected = H5Fopen("C:\\Users\\Danne\\raw_data\\cubi_cusco\\cellbender\\hub_01\\hub_01_cellbender_corrected.h5")
+corrected = H5Fopen("C:\\Users\\Danne\\raw_data\\cubi_cusco\\cellbender\\hub_02\\hub_02_cellbender_corrected.h5")
 cell_prob_tbl <- tibble(
   cell=corrected$metadata$barcodes_analyzed,
   cell_probability= corrected$droplet_latents$cell_probability)
@@ -241,8 +237,8 @@ cell_prob_vec <- cell_prob_tbl |> pull(cell_probability)
 names(cell_prob_vec) <- cell_prob_tbl |> pull(cell)
 seurat_obj <- AddMetaData(seurat_obj, col.name = "cellbender_prob_to_be_cell", cell_prob_vec)
 
-write_rds(seurat_obj, "intermediate_data/QC_dataset_setup_hub_01.rds")
-###-----------------------------------------------------------------------------------
+write_rds(seurat_obj, "intermediate_data/QC_dataset_setup_hub_02.rds")
+###-----------------------------------------------------------------------------------------------------
 
 
 
@@ -262,13 +258,17 @@ seurat_obj.markers <- FindAllMarkers(seurat_obj,
                                      max.cells.per.ident = max_cells_ ,) |>
   as_tibble()
 
-write_rds(seurat_obj.markers, file = paste0("output/",
+write_rds(seurat_obj.markers,
+          file = paste0("output/",
+                                            dataset_name,
                                             "QC_cluster_markers_min.pct_",min_pct,
                                             "_logfc.threshold_",logfc_threshold,
                                             "_max.cells.per.ident_",max_cells_,
                                             ".rds"))
 
-write_csv(seurat_obj.markers, file = paste0("output/",
+write_csv(seurat_obj.markers,
+          file = paste0("output/",
+                                            dataset_name,
                                             "QCmarkers_min.pct_",min_pct,
                                             "_logfc.threshold_",logfc_threshold,
                                             "_max.cells.per.ident_",max_cells_,
@@ -276,7 +276,7 @@ write_csv(seurat_obj.markers, file = paste0("output/",
 
 
 
-
+###-----------------------------------------------------------------------------------------------------
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hub_cubi_cusco
+
+##meta_data
+### cell_line_cellranger -- match CMOxx to cell line name, adjusted feature_call column from cellranger multi
+### singlet_doublet_cellranger -- cellranger calls with levels doublet, singlet no cell(not called a cell from cellranger and therefore not in file per_sample_outs but in file multi>count)
+###adjusted feature_call column from cellranger multi
+### cell_line_doublet_cellranger --- like singlet_doublet_cellranger but with cell line names
